4aa935fa6fa6d4a43e7dbabc0fcf549fd9aac1fd
Samuel <fsamuelsmartins@gmail.com>
2022-06-14 08:54:13 -0300

    adding loading package

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:todo_list/view/screens/create_accont_screen.dart';
 import 'package:todo_list/view/screens/login_screen.dart';
 import 'package:todo_list/view/screens/splash_screen.dart';
@@ -12,5 +13,6 @@ void main(List<String> args) {
       primarySwatch: Colors.blue,
     ),
     home: const SplashScreen(),
+    builder: EasyLoading.init(),
   ));
 }
